b4cefcba81d43a6a9d17d5ef1b908846d273d5e9
AndreyLytvynov <mrjastonehome@gmail.com>
2023-08-07 18:21:36 +0300

    fix post

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -17,9 +17,9 @@ const Post: FC<PostProp> = ({ post }) => {
   return (
     <div
       key={post._id}
-      className="flex h-[320px] w-full flex-col gap-4 rounded-lg bg-card p-5 desktop:w-[592px]"
+      className="flex h-[325px] w-full flex-col gap-2 rounded-lg bg-card p-5 tablet:h-[320px] tablet:gap-4 desktop:w-[592px]"
     >
-      <div className="flex h-[75px] items-center gap-5 overflow-hidden border-b-2 border-primary text-base">
+      <div className="flex h-[75px] items-center gap-5 overflow-hidden border-b-2 border-primary tablet:text-base">
         <svg
           stroke="#ffe700"
           fill="#ffe700"
@@ -28,6 +28,7 @@ const Post: FC<PostProp> = ({ post }) => {
           height="60px"
           width="60px"
           xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-8 tablet:h-10 tablet:w-10"
         >
           <path d="M209 96h-65c-26.4 0-48 21.6-48 48v128c0 26.4 21.6 48 48 48h38.7c4.6 0 8.3 3.9 8 8.6-2.3 35.5-18.8 60.2-31.3 74.1-4.7 5.2-1 13.4 5.9 13.4h39.9c2.5 0 4.8-1.1 6.3-3.1 7.4-9.6 28.5-41.6 28.5-93.9v-192c0-17.8-13.3-31.1-31-31.1zM385 96h-65c-26.4 0-48 21.6-48 48v128c0 26.4 21.6 48 48 48h38.7c4.6 0 8.3 3.9 8 8.6-2.3 35.5-18.8 60.2-31.3 74.1-4.7 5.2-1 13.4 5.9 13.4h39.9c2.5 0 4.8-1.1 6.3-3.1 7.4-9.6 28.5-41.6 28.5-93.9v-192c0-17.8-13.3-31.1-31-31.1z"></path>
         </svg>
@@ -36,7 +37,7 @@ const Post: FC<PostProp> = ({ post }) => {
         <p>{post.company.length !== 0 ? post.company : 'no company'}</p>
       </div>
       <p
-        className="line-clamp-[6] text-justify text-[10px] tablet:text-xs"
+        className="line-clamp-[10] text-justify text-[10px] tablet:line-clamp-[6] tablet:text-xs"
         style={{ flexGrow: 2 }}
       >
         {post.text}
