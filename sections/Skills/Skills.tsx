b01755ba161cb7014619e6ddf9e8ded1f87ad9c0
AndreyLytvynov <mrjastonehome@gmail.com>
2024-01-30 15:50:16 +0200

    add snapgram

--- sections/Skills/Skills.tsx
+++ sections/Skills/Skills.tsx
@@ -9,7 +9,7 @@ const Skills: FC = () => {
     <section className="pt-10 tablet:pt-20" id="skills">
       <div className="container">
         <h2 className="mb-10 text-2xl font-bold text-white">
-          Professional Skills
+          Using skills in my projects
         </h2>
         <ul className="flex flex-wrap justify-between gap-y-5">
           {data.skills.map(skill => (
